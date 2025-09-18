20f0d09ba4de9e7ba314326bbefac7d8bc0ddb9a
Peter <pcrall04@gmail.com>
2025-09-17 20:51:11 -0700

    Added textures

--- src/client/java/com/peter/ccgraphics/rendering/gui/GraphicsComputerScreen.java
+++ src/client/java/com/peter/ccgraphics/rendering/gui/GraphicsComputerScreen.java
@@ -1,9 +1,11 @@
 package com.peter.ccgraphics.rendering.gui;
 
+import com.peter.ccgraphics.CCGraphics;
 import com.peter.ccgraphics.computer.GraphicsComputerMenu;
 
 import dan200.computercraft.client.gui.AbstractComputerScreen;
 import dan200.computercraft.client.gui.GuiSprites;
+import dan200.computercraft.client.gui.GuiSprites.ComputerTextures;
 import dan200.computercraft.client.gui.widgets.TerminalWidget;
 import dan200.computercraft.core.util.Nullability;
 import net.minecraft.client.gl.RenderPipelines;
@@ -25,12 +27,14 @@ public class GraphicsComputerScreen extends AbstractComputerScreen<GraphicsCompu
         return new GraphicsScreenWidget(this.handler, input, this.x + 17 + 12, this.y + 12);
     }
 
+    private static final Identifier GRAPHICS_BORDER = CCGraphics.id("gui/border_graphics");
+    private static final Identifier GRAPHICS_SIDEBAR = CCGraphics.id("gui/sidebar_graphics");
+
     @Override
     protected void drawBackground(DrawContext graphics, float delta, int mouseX, int mouseY) {
         TerminalWidget terminal = this.getTerminal();
-        GuiSprites.ComputerTextures computerTextures = GuiSprites.getComputerTextures(this.family);
-        graphics.drawGuiTexture(RenderPipelines.GUI_TEXTURED, computerTextures.border(), terminal.getX() - 12, terminal.getY() - 12, terminal.getWidth() + 24, terminal.getHeight() + 24);
-        graphics.drawGuiTexture(RenderPipelines.GUI_TEXTURED, (Identifier)Nullability.assertNonNull(computerTextures.sidebar()), this.x, this.y + this.sidebarYOffset, 17, 38);
+        graphics.drawGuiTexture(RenderPipelines.GUI_TEXTURED, GRAPHICS_BORDER, terminal.getX() - 12, terminal.getY() - 12, terminal.getWidth() + 24, terminal.getHeight() + 24);
+        graphics.drawGuiTexture(RenderPipelines.GUI_TEXTURED, GRAPHICS_SIDEBAR, this.x, this.y + this.sidebarYOffset, 17, 38);
     }
 
 }
